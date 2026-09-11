d7f9ab2a3c75f1e5b79c908a86e3dd17ceb7a835
policyscope6-lab <policyscope6@gmail.com>
2026-09-12 01:42:06 +0500

    container config done

--- script.js
+++ script.js
@@ -7,3 +7,6 @@ console.log("This is a test script.");
 
 console.log("The value of a is: " + a);
 console.log("The square of a is: " + (a * a));
+
+console.log("The cube of a is: " + (a * a * a));
+console.log("The fourth power of a is: " + (a * a * a * a));
